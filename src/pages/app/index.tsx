3afb3849a01a1b514e6c985ebbc390da0046df35
Hirendra <stackwork87@gmail.com>
2021-07-19 10:07:58 +0530

    home page ui updated

--- src/pages/app/index.tsx
+++ src/pages/app/index.tsx
@@ -344,10 +344,15 @@ export default function Dashboard() {
                                         setTranscript(row["transcript"]);
                                       }}
                                     >
+                                      <div className="flex">
+                                        {row["title"] ? <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M20 7l-8-4-8 4m16 0l-8 4m8-4v10l-8 4m0-10L4 7m8 4v10M4 7v10l8 4" />
+                                        </svg> : null}
                                       <div className="text-sm font-medium text-gray-900">
                                         {row["title"]
                                           ? row["title"]
                                           : "(untitled)"}
+                                      </div>
                                       </div>
                                     </button>
                                   </Link>
@@ -418,7 +423,10 @@ export default function Dashboard() {
                                       }}
                                     >
                                       <>
-                                        <div>{row["status"]}</div>
+                                        <div className = "flex">
+                                          <div className="rounded-full w-2 h-2 bg-green-600 px-2 py-2 mt-1 mr-2"></div>
+                                          {row["status"]}
+                                          </div>
                                         <div>
                                           Render Date: {row["renderdate"]}
                                         </div>
